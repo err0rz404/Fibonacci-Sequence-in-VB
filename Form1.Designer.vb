f72d3b5da2e22d89e7fe4e3ba10f8c7b52776fa7
err0rz404 <43374920+err0rz404@users.noreply.github.com>
2019-01-29 15:56:21 +0000

    The project
    This is everything that i needed to do in class for the Fibonacci Sequence in VB hope its useful to somone

---
+++ Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GO = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'GO
+        '
+        Me.GO.Location = New System.Drawing.Point(108, 73)
+        Me.GO.Name = "GO"
+        Me.GO.Size = New System.Drawing.Size(75, 23)
+        Me.GO.TabIndex = 0
+        Me.GO.Text = "Go!"
+        Me.GO.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(95, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(1, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(304, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "How meny numbers do you want to the Fibbonacii sequence to"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(4, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(301, 66)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Answer appears here"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(303, 174)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GO)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GO As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub GO_Click(sender As Object, e As EventArgs) Handles GO.Click
+        Dim x As Integer = 0
+        Dim z As Integer = 1
+        Dim fibo As Integer = 0
+        Dim int As Integer = 0
+        If TextBox1.Text = "" Then
+            MsgBox("please input numbers into the text box")
+        Else
+            int = TextBox1.Text
+        End If
+        Label2.Text = ""
+        If int > 0 Then
+            Label2.Text = Label2.Text + x.ToString + ", "
+            If int > 1 Then
+                Label2.Text = Label2.Text + z.ToString + ", "
+            End If
+        End If
+        int = int - 2
+        While int > 0
+            fibo = x + z
+            x = z
+            z = fibo
+            Label2.Text = Label2.Text + fibo.ToString + ", "
+            int = int - 1
+        End While
+    End Sub
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If Not IsNumeric(Chr(e.KeyCode)) Then
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+End Class
